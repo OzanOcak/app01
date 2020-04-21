5e292d0e4bd8a0f4c04ba22aa5dda933b765b63b
OzanOcak <ozanocak@gmail.com>
2020-04-20 20:43:46 -0400

    automatic serialization

---
+++ lib/Message.dart
@@ -0,0 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'Message.g.dart';
+
+@JsonSerializable()
+class Message{
+  final String subject;
+  final String body;
+
+  Message(this.subject, this.body);
+
+  factory Message.fromJson(Map<String,dynamic>json) =>
+      _$MessageFromJson(json);
+} 
